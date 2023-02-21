2c9dbf9feda6ea7ea6b6e0eca6b4c1bc76822507
Airto <airto@intermixmanagement.nl>
2023-02-21 15:34:12 +0100

    created in create account mail and repeat password and some styling

--- resources/js/views/createAccount.vue
+++ resources/js/views/createAccount.vue
@@ -2,9 +2,21 @@
     <section class="create-account">
         <h1 class="create-account__title">CREATE ACCOUNT</h1>
         <form action="" method="post">
-            <input type="text" name="username">
-            <br><br>
-            <input type="password" name="password">
+            <div>
+                <label for="username">name:</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <label for="mail">mail:</label>
+                <input type="mail" name="mail"> 
+            </div>  
+            <div>
+                <label for="password">password:</label>       
+                <input type="password" name="password">
+                <br>
+                <label for="repeatPassword">repeat password:</label>
+                <input type="password" name="repeatPassword">
+            </div>
             <br><br>
             <input type="button" value="Create Account">
         </form>
