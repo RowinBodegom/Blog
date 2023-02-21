465c0bfc771183c3de7ad53e732260affffd3f18
Airto <airto@intermixmanagement.nl>
2023-02-21 16:28:36 +0100

    addad css for login

--- resources/js/views/login.vue
+++ resources/js/views/login.vue
@@ -1,14 +1,25 @@
 
 <template>
     <section class="login">
-        <h1 class="login__title">LOGIN</h1>
+        <h1 class="login__title">INLOGGEN</h1>
         <form action="" method="post">
-            <input type="text" name="username">
-            <br><br>
-            <input type="password" name="password">
-            <br><br>
-            <input type="button" value="Login">
+            <div class="login__input">
+                <input type="text" name="username" placeholder="Gebruikersnaam">
+            </div>
+            <div class="login__input">
+                <input type="password" name="password" placeholder="Wachtwoord">
+            </div>
+            <div class="login__input">
+                <input type="checkbox" name="privacycheck"><label for="privacycheck">Ja, ik ga akkoord met de privacyverklaring</label>
+            </div>
+            <div class="login__inlog-button">
+                <input type="button" value="Inloggen">
+            </div>
         </form>
-        <router-link to="/">Go to Home</router-link>
+
+        <section class="login__account-buttons">
+            <p><router-link to="#">Wachtwoord vergeten</router-link></p>
+            <p><router-link to="/createAccount">nog geen account?</router-link></p>
+        </section>
     </section>
 </template>
