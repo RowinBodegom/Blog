60de887e3882eb741c7f699dd7e7d1b9c28f4b92
Airto <airto@intermixmanagement.nl>
2023-02-24 15:59:27 +0100

    created a profile page and some css
    profile page is created with some styling to see the positioning of the profile page

---
+++ resources/js/views/profilePage.vue
@@ -0,0 +1,30 @@
+<template>
+        <img class="banner" :src="banner"/>
+        <div class="profilePage">
+            <div class="user">
+                <img class="user--picture" :src="person"/>
+                <div>
+                    <h1>gvfdkljgfdbjkf</h1>
+                </div>
+            </div>
+            <div class="blog">
+                <div class="blog__createblog">
+                    <img class="blog__createblog--picture" :src="logo"/>
+                    <input class="blog__createblog--input" type="text">
+                </div>
+                <div class="blog__showblog">
+                    <div v-for="n in 2" class="blog__showblog-card">
+                        <img class="blog__showblog-card--picture" :src="person"/>
+                        <h2>naam persoon</h2>
+                        <img class="blog__showblog-card--bigpicture" :src="person"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script setup>
+    import banner from "../../assets/luca-bravo-O453M2Liufs-unsplash.jpg";
+    import person from "../../assets/tyler-nix-PQeoQdkU9jQ-unsplash.jpg";
+    import logo from "../../assets/writing.png";
+</script>
