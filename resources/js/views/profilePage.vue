9e8c0001ce8fdae692e00308d7b59dc02593e41b
Airto <airto@intermixmanagement.nl>
2023-02-27 10:29:00 +0100

    added some elements updated scss

--- resources/js/views/profilePage.vue
+++ resources/js/views/profilePage.vue
@@ -10,12 +10,18 @@
             <div class="blog">
                 <div class="blog__createblog">
                     <img class="blog__createblog--picture" :src="logo"/>
-                    <input class="blog__createblog--input" type="text">
+                    <p @click="clickHandler()">schrijf een blog</p>
+                    <div class="blog__createblog-container" id="createblogContainer">
+                        <label for="">titel blog</label><input type="text">
+                        <label for="">text blog</label><input type="text">
+                        <label for="">foto blog</label><input type="file">
+                        <button>make blog</button>
+                    </div>
                 </div>
                 <div class="blog__showblog">
                     <div v-for="n in 2" class="blog__showblog-card">
                         <img class="blog__showblog-card--picture" :src="person"/>
-                        <h2>naam persoon</h2>
+                        <h2></h2>
                         <img class="blog__showblog-card--bigpicture" :src="person"/>
                     </div>
                 </div>
@@ -27,4 +33,14 @@
     import banner from "../../assets/luca-bravo-O453M2Liufs-unsplash.jpg";
     import person from "../../assets/tyler-nix-PQeoQdkU9jQ-unsplash.jpg";
     import logo from "../../assets/writing.png";
+</script>
+
+<script>
+export default {
+    methods: {
+        clickHandler(){
+            document.getElementById("createblogContainer").classList.toggle("blog__createblog-container--show");
+        }
+    }
+}
 </script>
