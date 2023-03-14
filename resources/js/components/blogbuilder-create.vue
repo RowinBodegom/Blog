ff10b0511e391687e19f2821358f37523fc8e320
Rowin Bodegom <rowin@intermixmanagement.nl>
2023-03-14 11:24:15 +0100

    added some new images, created a new component and added it to blogdetails

---
+++ resources/js/components/blogbuilder-create.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="blogbuilder-create">
+        <div class="blogbuilder-create__button">
+            <img class="blogbuilder-create" src="../../assets/write-text.png">
+            <img class="blogbuilder-create" src="../../assets/add-image.png">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "blogbuilder-create",
+}
+</script>
